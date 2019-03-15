66e673ffad63751f89b68e8a6675a20b00436e7d
Brendan Richards <brendanrichards@ssw.com.au>
2019-03-15 14:05:37 +1000

    change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'fbc-root',
@@ -8,5 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'firebootcamp-crm';
   now = Date();
-
 }
--- src/app/company/company-table/company-table.component.ts
+++ src/app/company/company-table/company-table.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Company } from '../company';
 
 @Component({
   selector: 'fbc-company-table',
   templateUrl: './company-table.component.html',
-  styleUrls: ['./company-table.component.scss']
+  styleUrls: ['./company-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CompanyTableComponent implements OnInit  {
 
@@ -23,4 +24,5 @@ export class CompanyTableComponent implements OnInit  {
   deleteCompany(company: Company) {
     this.deleteClicked.emit(company);
   }
+
 }
